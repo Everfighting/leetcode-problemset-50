273b56768d465ac3f3cf42cbc793b56b1578d2ef
binbin.cai <binbin.cai@alphainsight.ai>
2019-06-19 20:35:47 +0800

    two sum

---
+++ two_sum.py
@@ -0,0 +1,42 @@
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+
+
+# 法一：暴力求解
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                while nums[i] + nums[j] == target:
+                    return [i,j]
+                continue
+
+
+# 法二：hashtable
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        checked = dict()
+        for index,item in enumerate(nums):
+            if item in checked.keys():
+                return [checked[item], index]
+            checked[target-item] = index
+        return [-1, -1]
